e2ea3bcb5eb889b5e685e035e4b9a0766c45bc34
sldksns <sasha.mamontov.2008@gmail.com>
2026-01-19 23:54:49 +0500

    сделал профиль на процентов 80%, еще другие страницы изменил немного

--- src/main/java/org/example/burgerprime/controllers/AccountController.java
+++ src/main/java/org/example/burgerprime/controllers/AccountController.java
@@ -2,13 +2,18 @@ package org.example.burgerprime.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.burgerprime.interfaces.AccountRepository;
 import org.example.burgerprime.models.Account;
 import org.example.burgerprime.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.net.Authenticator;
 
 @AllArgsConstructor
 @Controller
@@ -16,7 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AccountController {
 
     private final UserService userService;
-
+    private final AccountRepository accountRepository;
     @GetMapping("/login")
     public String login() {
         return "login";
@@ -35,6 +40,16 @@ public class AccountController {
             return "reg";
         }
         userService.createUser(user);
+        return "redirect:/login";
+    }
+    @PostMapping("/profile/edit_name")
+    public String editName(Authentication authentication, String new_name) {
+        Object principal = authentication.getPrincipal();
+        String username = ((UserDetails) principal).getUsername();
+        Account account = accountRepository.findByName(username);
+        account.setName(new_name);
+        accountRepository.save(account);
+        log.info("User {} changed name to {}", username, new_name);
         return "redirect:/login";
     }
 }
